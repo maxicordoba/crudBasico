a5ef1d110356def929b218ed907dc441cb15c42b
Maxi <maxi_cordoba32@hotmail.com>
2021-10-15 17:44:30 -0300

    se agregaron todas las validaciones del formulario de productos

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,29 +1,93 @@
-function validarCampoRequerido (input){
-   // console.log(input);
-    console.log(input.value);
-    if(input.value.trim().length > 0 && input.value.trim().length >= 3){
-        console.log("el dato es correcto");
-        input.className = "form-control is-valid"
-    }else{
-        console.log("dato erroneo");
-        input.className = "form-control is-invalid"
-    }
+function validarCampoRequerido(input) {
+  // console.log(input);
+  console.log(input.value);
+  if (input.value.trim().length > 0 && input.value.trim().length >= 3) {
+    console.log("el dato es correcto");
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    console.log("dato erroneo");
+    input.className = "form-control is-invalid";
+    return false;
+  }
 }
 
-function validarNumeros(input){
-    //validar con expresiones regulares
-    let patron = /^[0-9]{1,5}$/;
-    if(patron.test(input.value)){
-        input.className = "form-control is-valid";
-    }else{
-        input.className = "form-control is-invalid";
-    }
+function validarNumeros(input) {
+  //validar con expresiones regulares
+  let patron = /^[0-9]{1,5}$/;
+  if (patron.test(input.value)) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+
+function validarCodigo(input) {
+  // validar que tenga al menos 3 caracteres
+  if (input.value.trim() != "" && input.value.trim().length >= 3) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+
+function validarURL(input) {
+  let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+  if (input.value.trim() != "" && patron.test(input.value.trim())) {
+    input.className = "form-control is-valid";
+    return true;
+  } else {
+    input.className = "form-control is-invalid";
+    return false;
+  }
+}
+
+function validarGeneral(e) {
+  e.preventDefault();
+  //console.log("desde validar general");
+  //console.log(e);
+  let alerta = document.querySelector("#msjAlerta");
+  if (
+    validarCodigo(document.querySelector("#codigo")) &&
+    validarCampoRequerido(document.querySelector("#producto")) &&
+    validarCampoRequerido(document.querySelector("#descripcion")) &&
+    validarNumeros(document.querySelector("#cantidad")) &&
+    validarURL(document.querySelector("#url"))
+  ) {
+    console.log("validacion correcta");
+    alerta.className= "alert alert-danger mt-4 d-none";
+  } else {
+    console.log("validacion incorrecta");
+    alerta.className = "alert alert-danger mt-4";
+  }
 }
 
 let producto = document.querySelector("#producto");
 let cantidad = document.querySelector("#cantidad");
-//console.log(cantidad);
+let codigo = document.querySelector("#codigo");
+let descripcion = document.querySelector("#descripcion");
+let url = document.querySelector("#url");
+let formulario = document.querySelector("#formProducto");
+//console.log(formulario);
 
 //agregar eventos desde javascript
-producto.addEventListener("blur", ()=>{validarCampoRequerido(producto)});
-cantidad.addEventListener("blur", ()=>{validarNumeros(cantidad)});
+producto.addEventListener("blur", () => {
+  validarCampoRequerido(producto);
+});
+cantidad.addEventListener("blur", () => {
+  validarNumeros(cantidad);
+});
+descripcion.addEventListener("blur", () => {
+  validarCampoRequerido(descripcion);
+});
+codigo.addEventListener("blur", () => {
+  validarCampoRequerido(codigo);
+});
+url.addEventListener("blur", () => {
+  validarCampoRequerido(url);
+});
+formulario.addEventListener("submit", validarGeneral);
